d71ba854006fe7fec5a67c1c35bf95a5bb64b010
Kaushik Chakraborty <75164835+Master757@users.noreply.github.com>
2026-03-19 21:43:45 +0530

    Update main.py
    added reminders and threding

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from Brains import introduce, listen, exit, clean_command, agent_decide_and_act
 from VoicePartitions import talk 
 from SpeechRec import listen_for_speech
 from agent_tools import emergency_alert
+import threading
+from agent_tools import REMINDERS
+import datetime
 
-introduce()
+def reminder_checker():
+    #Background thread to monitor reminders.
+    while True:
+        now = datetime.datetime.now().strftime("%H:%M")
+        for r in REMINDERS:
+            if r["time"] == now and not r["done"]:
+                print(f"[ALERT] Reminder: {r['task']}")
+                talk(f"Excuse me, I have a reminder for you: {r['task']}")
+                r["done"] = True
+        time.sleep(10) # Check every 10 seconds
 
 def activate_assistant():
     """
@@ -38,6 +50,9 @@ def activate_assistant():
                 print("[Error]: ", e)
 
 def main():
+    clock_thread = threading.Thread(target=reminder_checker, daemon=True)
+    clock_thread.start()
+    introduce()
     while True:
         spoken_text = listen_for_speech().lower()
         if spoken_text == "":
@@ -49,7 +64,7 @@ def main():
         if "help me" in spoken_text or "send emergency msg" in spoken_text:
             print("Emergency words detected...")
             emergency_alert("son")
-            
+
         if "hey violet" in spoken_text:
             print(" Activated")
             should_continue = activate_assistant()
